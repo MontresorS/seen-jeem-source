304266465e2cfbad14bc148c7d4a5a7ad055ac23
MontresorS <montaser.sharshar@gmail.com>
2026-08-08 17:56:38 +0300

    Make 200-point tiles visible

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./moving-letters.css";
+import "./points-visibility.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
